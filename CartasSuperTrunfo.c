54c1bb6d43eaff05b616559168d818b4c5abc6d2
Mateus <mateusvianna.ai@gmail.com>
2025-09-17 23:19:34 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,39 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+   int populacao, turismo;
+    char inicial = 'E';
+    char nome[20];
+    float area;
+    float pib;
+    char codigo[30];
   // Área para entrada de dados
+  printf("Código da Carta: \n");
+    scanf("%s", &codigo);
 
+    printf("Nome da Cidade:\n");
+    scanf("%s", &nome);
+
+    printf("Número da População: \n");
+    scanf("%d", &populacao);
+
+    printf("Área em Km²:\n");
+    scanf("%f", &area);
+
+    printf("PIB:\n");
+    scanf("%f", &pib);
+
+    printf("Pontos Turísticos:\n");
+    scanf("%d", &turismo);
   // Área para exibição dos dados da cidade
-
+    printf("Carta 1:\n");
+    printf("Estado: %c\n", inicial);
+    printf("Código: %s\n", codigo);
+    printf("Nome da Cidade: %s\n", nome);
+    printf("População: %d\n", populacao);
+    printf("Área: %fKm²\n", area);
+    printf("PIB: %fbilhões de reais\n", pib);
+    printf("Número de Pontos Turísticos: %d\n", turismo);
+  
 return 0;
 } 
