2cec5d3d34cea12ad5eca6197c515c19c24ee38e
Mateus <mateusvianna.ai@gmail.com>
2025-09-24 01:34:24 -0300

    Add files via upload
    Atualização de funcionalidades

--- Super_Trunfo.c
+++ Super_Trunfo.c
@@ -55,15 +55,30 @@ int main(){
     printf("Pontos Turísticos:\n");
     scanf("%d", &turismo2);
 
+/*Adicionando operadores matemáticos para os cálculos de
+ densidada populacional e PIB per capita da carta1.*/
+
+    float densidade = (float) populacao / area;
+    double pibpercapita = pib / populacao;
+
 //Resultado do cadastramento da Carta 1:
     printf("Carta 1:\n");
     printf("Estado: %c\n", inicial);
     printf("Código: %s\n", codigo);
     printf("Nome da Cidade: %s\n", nome);
     printf("População: %d\n", populacao);
-    printf("Área: %fKm²\n", area);
-    printf("PIB: %fbilhões de reais\n", pib);
+    printf("Área: %.2fKm²\n", area);
+    printf("PIB: %.2fbilhões de reais\n", pib);
     printf("Número de Pontos Turísticos: %d\n", turismo);
+    printf("Densidade Populacional:  %.2fhab/Km²\n", densidade);
+    printf("PIB per Capita: %.2f milhões de reais\n", pibpercapita);
+
+/*Adicionando operadores matemáticos para os cálculos de
+ densidada populacional e PIB per capita da carta2.*/
+
+    float densidade2 = (float) populacao2 / area2;
+    double pibpercapita2 = pib2 / populacao2;
+
 
     //Resultado do cadastramento da Carta 2:
     printf("Carta 2:\n");
@@ -71,9 +86,12 @@ int main(){
     printf("Código: %s\n", codigo2);
     printf("Nome da Cidade: %s\n", nome2);
     printf("População: %d\n", populacao2);
-    printf("Área: %fKm²\n", area2);
-    printf("PIB: %fbilhões de reais\n", pib2);
+    printf("Área: %.2fKm²\n", area2);
+    printf("PIB: %.2fbilhões de reais\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", turismo2);
+    printf("Densidade Populacional:  %.2fhab/Km²\n", densidade2);
+    printf("PIB per Capita: %.2f milhões de reais\n", pibpercapita2);
+
 
     return 0;
 }
